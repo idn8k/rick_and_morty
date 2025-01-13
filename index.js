8568d0ae208818ba68a66af9d24a040c4a7f3bee
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-13 14:39:27 +0100

    fetch - done

--- index.js
+++ index.js
@@ -9,7 +9,28 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const URL = 'https://rickandmortyapi.com/api/character';
+
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = '';
+
+async function fetchCharacters() {
+   try {
+      const response = await fetch(URL);
+      const data = await response.json();
+
+      if (!response.ok) {
+         console.log('Something went wrong...!');
+      }
+
+      return data.results;
+   } catch (error) {
+      console.log('ERROR:', error);
+   }
+}
+
+const chars = await fetchCharacters();
+
+createCharacterCard(chars);
